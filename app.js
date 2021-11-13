439d91be63bc7edc84d61392cafb001084807ede
Raashot12 <akannirasheed1996@gmail.com>
2021-11-13 16:56:38 +0100

    profile updated

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+//Find the first ascending integer that does not exist in an array of numbers.
+
+Ex: [1, 2, 3, 5, 9, 2, 3]
+//You should find 4.
+
+//How do you do it ?
+
+// const sortedArr = arr.sort()
+// const nonRepeatedNos = new Set( sortedArr )
+// const array = [...nonRepeatedNos]
+// const result = array.reduce( ( p, c, i ) => {
+// 	if ( c !== i ) {
+// 		return i
+// 	}
+// } )
